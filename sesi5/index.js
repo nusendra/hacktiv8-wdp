bd4efdecdfef984f1ea5ba96398bf25a71474563
Nusendra Hanggarawan <admin@nusendra.com>
2024-04-17 16:53:08 +0700

    menambahkan sesi 5

---
+++ sesi5/index.js
@@ -0,0 +1,55 @@
+let namaDepan = "ahmad"; // string
+let nama_belakang = "fani"; // string
+let angka = 1; // integer / number
+
+let nama = "123";
+console.log(nama + nama);
+
+// let = variable yang bisa / boleh diubah
+// const = variable yang datanya tidak boleh / tidak bisa diubah
+
+let nomor = 1;
+nomor = 2;
+
+console.log(nomor);
+
+const NOMOR_X = 5;
+console.log(NOMOR_X);
+
+// ---
+
+const keluarga = ["bapak", "ibuk", "adik", "kakak"]; // Array
+// Array = sebuah kumpulan data;
+
+console.log(keluarga);
+console.log(keluarga[2]);
+
+// ----
+const manusia = {
+  nama: "fani",
+  umur: 25,
+  alamat: "surabaya",
+  email: "blabla@blabla.net",
+  hp: ["iphone", "android"],
+};
+
+const parkiran = [
+  {
+    nama: "mobil 1",
+    roda: 4,
+    pintu: 4,
+    penumpang: ["supir", "pengamen", "orang biasa"],
+  },
+  {
+    nama: "mobil 2",
+    roda: 3,
+    pintu: 2,
+  },
+  {
+    nama: "bis",
+    roda: 6,
+    pintu: 2,
+  },
+];
+
+console.log(parkiran);
